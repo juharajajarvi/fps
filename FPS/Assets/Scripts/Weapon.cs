9a0d14599a31f02a3946cd3a13c086d9c5000fb9
Juha Rajajarvi <juhraj@utu.fi>
2013-01-28 15:44:45 +0200

    First version of muzzle flash working.

--- FPS/Assets/Scripts/Weapon.cs
+++ FPS/Assets/Scripts/Weapon.cs
@@ -7,12 +7,16 @@ public class Weapon : MonoBehaviour
 	public Rigidbody bulletPrefab;
 	private float minWaitTime;
 	private float currentIdleTime;
+	public ParticleEmitter muzzleFlash;
+	private int muzzleFlashLeft; // TODO make private
 	
 	// Use this for initialization
 	void Start () 
 	{
+		//muzzleFlash = GetComponentInChildren(ParticleEmitter);
 		minWaitTime = 0.1f; // 0.3 seconds
 		currentIdleTime = minWaitTime + 1f;
+		muzzleFlashLeft = 0;
 	}
 	
 	// Update is called once per frame
@@ -26,13 +30,30 @@ public class Weapon : MonoBehaviour
 		}
 	}
 	
-	public void Shoot(Transform transform) 
+	void LateUpdate() { 
+		if ( muzzleFlashLeft > 0) {
+			muzzleFlash.emit = true;
+			muzzleFlashLeft--;
+			/*if (audio) {
+				if (!audio.isPlaying) {
+					audio.Play ();
+					audio.loop = true;
+				}
+			}*/
+		}
+		else {
+			muzzleFlash.emit = false;
+		}
+	}
+	
+	public void Shoot(Transform transform)
 	{
 		if ( ! isGunReady() ) 
 		{
 			return;
 		}
 		
+		muzzleFlashLeft = 1;
 		currentIdleTime = 0f;
 		
 	    // Instantiate the projectile at the position and rotation of this transform
