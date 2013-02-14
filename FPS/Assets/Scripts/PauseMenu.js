b7ea3673d69563551fec10f54aadaca7ba19162e
Juha Rajajarvi <juhraj@utu.fi>
2013-02-14 10:09:45 +0200

    Pause menu stuff

--- FPS/Assets/Scripts/PauseMenu.js
+++ FPS/Assets/Scripts/PauseMenu.js
@@ -1,5 +1,11 @@
 #pragma strict
 
+public var playerController:PlayerController;
+
+// This would stop the player from looking around when on pause mode,
+// but can't find that script!
+//public var playerLook:PlayerLook;
+
 var MenuActive:boolean = false;
 
 function Start () {
@@ -11,58 +17,57 @@ function Update () {
 }
 
 function OnGUI () { 
-
+	
 	if (MenuActive == true) {      
-	
-	    //show mouse cursor
-	
-	    Screen.showCursor = true;}
-	
-	    else {Screen.showCursor = false;}
-	
-	    
-	
-	    if (MenuActive == true){
-	
-	    // Make a background box 
-	
-	   GUI.Box (Rect ((Screen.width-200)/2,80,200,180), "CrateSlinger"); 
-	
-	   //First button returns to the game in progress
-	
-	   if (GUI.Button (Rect ((Screen.width-150)/2,110,150,20), "Back to game")) { 
-	
-	      MenuActive=!MenuActive;
-	
-	      }
-	
-	      //Second button returns to main menu
-	
-	       if (GUI.Button (Rect((Screen.width-150)/2,190,150,20), "Main Menu")) { 
-	
-	      Application.LoadLevel ("menu"); 
-	
-	   } 
-	
-	   } 
-
- 
-} 
-
- 
-
-function LateUpdate()
-
-{
-
- if (Input.GetButtonDown("Escape")) { 
-
-      MenuActive=!MenuActive; 
-
-      }
-
-    if(MenuActive == true) { 
-
-     print ("Escape pushed");
+		//show mouse cursor
+		Screen.showCursor = true;
 	}
+	else {
+		Screen.showCursor = false;
+	}
+	
+	if (MenuActive == true) {
+		
+		// Make a background box 
+		GUI.Box (Rect ((Screen.width-200)/2,80,200,180), "ALIEN FPS -- PAUSED"); 
+	
+		//First button returns to the game in progress
+		if (GUI.Button (Rect ((Screen.width-150)/2,110,150,20), "Back to game")) { 
+			MenuActive = false;
+			Resume();
+		}
+	
+		//Second button returns to main menu
+		if (GUI.Button (Rect((Screen.width-150)/2,190,150,20), "Quit")) { 
+			Application.Quit(); 
+		}
+	}
+	
+}
+
+
+function LateUpdate() {
+
+	if (Input.GetButtonDown("Escape")) {
+		MenuActive=!MenuActive;
+	}
+	
+	if (MenuActive) {
+		SetPaused();
+	} else {
+		Resume();
+	}
+}
+
+function SetPaused() {
+	Time.timeScale = 0.0;
+	playerController.enabled = false;
+	//playerLook.enabled = false;
+	Debug.Log("Paused");
+}
+
+function Resume() {
+	Time.timeScale = 1.0;
+	playerController.enabled = true;
+	//playerLook.enabled = true;
 }
